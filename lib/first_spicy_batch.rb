61820a554d1ef525fe30ff662ad73b6a65cf3a7b
Jeff Marks <jeff.t.marks@gmail.com>
2022-11-12 14:58:41 -0500

    done with assignment

--- lib/first_spicy_batch.rb
+++ lib/first_spicy_batch.rb
@@ -3,6 +3,33 @@
 # @return {boolean} whether the batch is spicy
 # def is_spicy_batch(batch):
 
-def first_spicy_batch(n)
-  # your code here
+require 'pry'
+
+@batch = [true, true, true, true, true, true, true]
+
+def is_spicy_batch(n)
+	@batch[n - 1]
 end
+
+def first_spicy_batch(n)
+	return n if n === 1
+
+  middle = (n.to_f / 2).ceil
+
+	if is_spicy_batch(middle)
+		if !is_spicy_batch(middle - 1)
+			return middle
+		else
+			return first_spicy_batch(middle)
+		end
+	else
+		if is_spicy_batch(middle + 1)
+			return middle + 1
+		else
+			return first_spicy_batch(n + middle)
+		end
+	end
+end
+
+
+puts first_spicy_batch(7)
