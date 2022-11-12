9cb94a0ba8a162fe41df244c46fe7ca7f581f37c
Jeff Marks <jeff.t.marks@gmail.com>
2022-11-12 14:31:27 -0500

    commit tree 840c9343014f28ed1db24f0e47269d5a15540560

--- lib/first_spicy_batch.rb
+++ lib/first_spicy_batch.rb
@@ -4,5 +4,33 @@
 # def is_spicy_batch(batch):
 
 def first_spicy_batch(n)
-  # your code here
+  middle = (n / 2).ceil
+
+	result = nil
+
+	# If middle of batch is spicy
+	if is_spicy_batch(middle)
+		# If previous batch is not spicy, you've found first spicy batch
+		if !is_spicy_batch(middle - 1)
+			return middle
+		# If previous batch is spicy, look to earlier batch
+		else
+			result = first_spicy_batch(middle)
+		end
+	# If middle of batch is not spicy
+	else
+		# If next batch is spicy, you've found first spicy batch
+		if is_spicy_batch(middle + 1)
+			return middle
+		else
+			result = first_spicy_batch(n + middle)
+		end
+
+
+	end
+
+	return result
+
 end
+
+puts first_spicy_batch(5);
